d2b427f0a347da994499eedbee6ed2c7b62e040c
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-07-25 22:44:49 -0600

    Programm Finish

--- Ejercicio/Program.cs
+++ Ejercicio/Program.cs
@@ -100,7 +100,7 @@ public class ArchaicCalculator
         Console.Clear();
         try
         {
-            Console.WriteLine("--------------------------");
+            Console.WriteLine("---------------------------");
             Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("       NEW NUMBERS");
             Console.ResetColor();
